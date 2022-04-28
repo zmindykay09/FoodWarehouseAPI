cb8acc65cd63c667ab17eddd38723e2f69c40a04
Mindy Kay V. Zaracena <mindykayzara05@gmail.com>
2022-04-28 14:39:45 +0800

    created a new model

---
+++ zaracena_sfEXAM/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ZaracenaSfexamConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'zaracena_sfEXAM'
---
+++ zaracena_sfEXAM/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+class foodstuff(models.Model):
+    Mfd_id = models.IntegerField(primary_key=True)
+    Name = models.CharField(max_length=60)
+    UnitPrice = models.DecimalField(max_digits=5, decimal_places=2)
+    UnitsInStock = models.PositiveSmallIntegerField()
+
+    def _str_(self):
+        return self.name
